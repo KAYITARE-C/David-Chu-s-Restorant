c0f565d94bae147d3d4df44257638f8021ad31b3
KAYITARE-C <ani7kayitare@gmail.com>
2021-10-27 06:54:15 +0200

    all

---
+++ bootstrap-3.3.7-dist/js/script.js
@@ -0,0 +1,12 @@
+$(function(){//ituma izi code ziba run nyuma ya html content ariko mbere ya image ni izindi external file .same as document.addEventListener("DOMContentLoader")
+    
+    //same as document.querySelector("#navbarToggle").addEventListener("blur",)
+    $("#navbarToggle").blur(function(event){
+        let screenWidth=window.innerWidth;
+        if(screenWidth<768){
+            $("#collapsable-nav").collapse("hide");
+
+        }
+
+    });
+});
